daaa3d0250f77b17cc2804c215ebd7d7cc6be413
letuan279 <latuan388@gmail.com>
2023-11-29 09:23:31 +0700

    test action

--- src/index.js
+++ src/index.js
@@ -4,19 +4,21 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 const cors = require("cors");
 app.use(cors());
 
-const {morganBodyConfig} = require("./logging/morgan-config");
+const { morganBodyConfig } = require("./logging/morgan-config");
 const morganBody = require("morgan-body");
 morganBody(app, morganBodyConfig);
 
 app.use("/api", require("./routes"));
 
-const {handleError} = require("./errors/handle-errors-middleware");
+const { handleError } = require("./errors/handle-errors-middleware");
 app.use(handleError);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+
+console.log("hehe");
